28d36b9206b463f22e797003b7ad2d70f957bea1
Abiyaruth joseph <abiyaruth@gmail.com>
2017-01-14 22:34:08 -0600

    Module3-solution

---
+++ module3-solution/scripts/controller.js
@@ -0,0 +1,34 @@
+(function(){
+    angular.module('NarrowItDownApp')
+        .controller('NarrowItDownController', NarrowItDownController)
+
+    NarrowItDownController.$inject = ['$scope', 'MenuSearchService']
+    function NarrowItDownController($scope, MenuSearchService) {
+      let self = this
+
+      self.found = []
+      self.searched = false
+
+      self.removeItem = function (itemIndex) {
+            self.found.splice(itemIndex, 1);
+      };
+
+      self.noItemsFound = function () {
+        return self.searched && self.found.length === 0
+      }
+
+      $scope.searchAPI = function () {
+        MenuSearchService.getMatchedMenuItems($scope.searchTerm)
+          .then(function (data) {
+           console.dir(data)
+            self.searched = true
+            self.found = data
+
+            if (data.isSkippingSearch)
+              $scope.$apply()
+          })
+      }
+
+    }
+
+})()
